b1aa12fa7cc371ea8eb567830ece79681d6f9b94
Dimitris_Dr <jidrist18@hotmail.com>
2024-10-23 23:32:51 +0300

    modified dialog box

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const dialogRef = useRef()
 
   function openDialog() {
-     dialogRef.current.showModal()
+    dialogRef.current.showModal()
   }
 
   function closeDialog() {
@@ -25,7 +25,7 @@ function App() {
   return (
     <>
       <main className="main grid-item" style={{'--gap':'1.5rem'}}>
-        <h1 className="main-title fw-700 fs-900">Desserts</h1>
+        <h1 className="main-title fw-500 fs-900">Desserts</h1>
         <section className="cards-container grid-item" aria-label='Product cards'>
           {data.map((item, i) => {return <Card
                                           key={i} 
@@ -37,7 +37,11 @@ function App() {
         </section>
       </main>
       <Cart cartData = {cartData} setCartData={setCartData} openDialog={openDialog} />
-      <Dialog cartData={cartData} setCartData={setCartData} dialogRef={dialogRef} closeDialog={closeDialog}/>
+      <Dialog cartData={cartData} 
+              setCartData={setCartData} 
+              dialogRef={dialogRef} 
+              closeDialog={closeDialog}
+      />
     </>
     
   )
--- src/dialog.jsx
+++ src/dialog.jsx
@@ -2,9 +2,16 @@ export default function Dialog({cartData, setCartData, dialogRef, closeDialog}) 
 
     return (
     <dialog ref={dialogRef} className="dialog">
-        <div className="dialog-content">
-            <p>Hello! I'm open by default without modal behavior</p>
-            <button className="btn close-btn" onClick={closeDialog} >Close Modal</button>
+        <div className="dialog-content grid-item">
+            <div className="title-container">
+                <img src="public\assets\images\icon-order-confirmed.svg" alt="confirmed order" className="img confirm-img" />
+            </div>
+            <div className="titles-container grid-item" style={{'--gap':'0.5rem'}}>
+                <h2 className="fw-700 fs-700 clr-rose-900">Order Confirmed</h2>
+                <p className="clr-rose-500">We hope you enjoy your food!</p>
+            </div>
+
+            <button className="btn close-btn clr-rose-50" onClick={closeDialog} >Start New Order</button>
         </div>
     </dialog>
     )
