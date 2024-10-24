72b704a072d01c8d13ac9366bb6c3459910cec4c
Dimitris_Dr <jidrist18@hotmail.com>
2024-10-24 17:59:37 +0300

    update css

--- src/cartItems.jsx
+++ src/cartItems.jsx
@@ -4,30 +4,32 @@ export default function CartItems({cartData, setCartData, dialogFunc, orderConfi
     const totalPrice = Object.keys(cartData).map(key => cartData[key].count*cartData[key].price).reduce((a,b) => a + b)
      return (
         <>
-            <div className="cart-items">
-                {Object.keys(cartData).map(key => {
-                    if (cartData[key].count === 0) return
-                    return <CartItem
-                     key={key}
-                     itemName={key} 
-                     cartData={cartData}
-                     setCartData={setCartData}
-                     orderConfirmed={orderConfirmed}
-                    />
-                })}
-            </div>
-            <div className="order-total flex-item">
-                <span className="order-total-txt clr-rose-500">Order Total</span>
-                <span className="order-total-num fs-900 fw-700 clr-rose-900">${totalPrice}</span>
-            </div>
-            {
-                orderConfirmed
-                 &&
-                 <div className="carbon-free-msg flex-item">
-                    <img src="public\assets\images\icon-carbon-neutral.svg" alt="carbon free icon" className="img" />
-                    <p className="carbon-free-msg-txt clr-rose-500">This is a <strong>carbon-neutral</strong> delivery</p>
+            <div className="cart-items-container grid-item">
+                <div className="cart-items">
+                    {Object.keys(cartData).map(key => {
+                        if (cartData[key].count === 0) return
+                        return <CartItem
+                        key={key}
+                        itemName={key} 
+                        cartData={cartData}
+                        setCartData={setCartData}
+                        orderConfirmed={orderConfirmed}
+                        />
+                    })}
                 </div>
-            }  
+                <div className="order-total flex-item">
+                    <span className="order-total-txt clr-rose-500">Order Total</span>
+                    <span className="order-total-num fs-700 fw-700 clr-rose-900">${totalPrice}</span>
+                </div>
+                {
+                    orderConfirmed
+                    &&
+                    <div className="carbon-free-msg flex-item">
+                        <img src="public\assets\images\icon-carbon-neutral.svg" alt="carbon free icon" className="img" />
+                        <p className="carbon-free-msg-txt clr-rose-500">This is a <strong>carbon-neutral</strong> delivery</p>
+                    </div>
+                }
+            </div>
             <button onClick={dialogFunc} className="btn confirm-order-btn clr-rose-50">{orderConfirmed?'Confirm Order': 'Start New Order'}</button>
         </>
      )
--- src/dialog.jsx
+++ src/dialog.jsx
@@ -9,7 +9,7 @@ export default function Dialog({cartData, setCartData, dialogRef, dialogFunc}) {
                 <img src="public\assets\images\icon-order-confirmed.svg" alt="confirmed order" className="img confirm-img" />
             </div>
             <div className="titles-container">
-                <h2 className="fw-700 fs-900 clr-rose-900">Order Confirmed</h2>
+                <h2 className="fw-700 fs-700 clr-rose-900">Order Confirmed</h2>
                 <p className="clr-rose-500">We hope you enjoy your food!</p>
             </div>
             <CartItems cartData={cartData} setCartData={setCartData} dialogFunc={dialogFunc} orderConfirmed={false} />
