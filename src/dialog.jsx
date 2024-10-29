7dfcd62a1ae61663acf5832fba7de2539b8f2d95
Dimitris_Dr <jidrist18@hotmail.com>
2024-10-29 13:52:36 +0200

    updated readme

--- src/dialog.jsx
+++ src/dialog.jsx
@@ -6,7 +6,7 @@ export default function Dialog({dialogRef, dialogFunc}) {
     <dialog ref={dialogRef} className="dialog">
         <div className="dialog-content grid-item">
             <div className="title-container">
-                <img src="public\assets\images\icon-order-confirmed.svg" alt="confirmed order" className="img confirm-img" />
+                <img src="/product-list-with-cart-react/assets/images/icon-order-confirmed.svg" alt="confirmed order" className="img confirm-img" />
             </div>
             <div className="titles-container">
                 <h2 className="fw-700 fs-700 clr-rose-900">Order Confirmed</h2>
