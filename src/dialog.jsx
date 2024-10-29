daa1ffa0e09d5285fe7a36f347b7f6aeb292c270
Dimitris_Dr <jidrist18@hotmail.com>
2024-10-29 19:16:48 +0200

    update fonts

--- src/dialog.jsx
+++ src/dialog.jsx
@@ -9,7 +9,7 @@ export default function Dialog({dialogRef, dialogFunc}) {
                 <img src="/product-list-with-cart-react/assets/images/icon-order-confirmed.svg" alt="confirmed order" className="img confirm-img" />
             </div>
             <div className="titles-container">
-                <h2 className="fw-700 fs-700 clr-rose-900">Order Confirmed</h2>
+                <h2 className="fw-700 fs-900 clr-rose-900">Order Confirmed</h2>
                 <p className="clr-rose-500">We hope you enjoy your food!</p>
             </div>
             <CartItems dialogFunc={dialogFunc} orderConfirmed={false} />
